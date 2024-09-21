61a441d71c0276e80d3df2a098a686cdc706e172
Kevin Le <121095365+MrGrinchFx@users.noreply.github.com>
2024-09-21 12:34:33 -0700

    basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,66 @@
 use std::env;
-use std::fs::File;
-use std::io::{self, Read};
+use std::io::Read;
 use std::process;
-
-const BUFFER_SIZE: usize = 16;
-
+use std::fs::File;
 fn main() {
-    println!("Hello, World!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 4 {
+        println!("Usage: hexdump -n LEN FILE");
+        process::exit(1);
+    }
+
+    if &args[1] != "-n" {
+        println!("Usage: hexdump -n LEN FILE");
+        process::exit(1);
+    }
+
+    let size: usize = match args[2].parse() {
+        Ok(n) => n,
+        Err(_) => {
+            println!("Invalid length");
+            process::exit(1);
+        }
+    };
+
+    let file_name: String = args[3].clone();
+
+    let mut file: File = match File::open(file_name) {
+        Ok(file) => file,
+        Err(_) => {
+            println!("Error opening file");
+            return;
+        }
+    };
+
+    let mut contents = Vec::new();
+    match file.read_to_end(&mut contents) {
+        Ok(_) => (),
+        Err(_) => {
+            println!("Error reading file");
+            return;
+        }
+    }
+
+    let mut offset: usize = 0;
+    for chunk in contents[..size].chunks(16) {
+        print!("{:08x}  ", offset); // Start from 0x000
+        for byte in chunk {
+            print!("{:02x} ", byte);
+        }
+        for _ in 0..(16 - chunk.len()) {
+            print!("   "); // Pad with spaces
+        }
+        // print!("| ");
+        // for byte in chunk {
+        //     if *byte >= 32 && *byte <= 126 {
+        //         print!("{}", *byte as char);
+        //     } else {
+        //         print!(".");
+        //     }
+        // }
+        // println!(" |");
+        println!(" ");
+        offset += chunk.len();
+    }
 }
